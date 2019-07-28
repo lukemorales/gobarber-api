63a1e72a46069f4092523df5db0a6ae2ab445da5
Luke Morales <lukemorales@live.com>
2019-07-28 15:25:39 -0300

    chore: Added Avatar data to User and Session Controllers

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -65,13 +66,23 @@ class UserController {
       return res.status(401).json({ error: 'Old Password does not match.' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userID, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
