6cc56e1fb0bad44ce3bd60b77e3afc0a89b2bcc4
Luke Morales <lukemorales@live.com>
2019-07-28 15:01:32 -0300

    chore: Added CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -22,7 +23,7 @@ class App {
   middlewares() {
     // The request handler must be the first middleware on the app
     this.server.use(Sentry.Handlers.requestHandler());
-
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
